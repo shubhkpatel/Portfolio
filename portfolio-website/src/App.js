9684e71e9e94ee0d79fce570eea1b30f7ba35ed4
Shubh <patelshubh694@gmail.com>
2021-12-30 11:34:30 +0530

    Created Global Styled and Black & White Themes

--- portfolio-website/src/App.js
+++ portfolio-website/src/App.js
@@ -1,9 +1,19 @@
+import { ThemeProvider } from "styled-components"
+import { lightTheme } from "./components/Themes";
+import GlobalStyle from "./globalStyles"
+
 function App() {
-  return <div>
-    App File
-    </div>
-    
+  return <>
+    <GlobalStyle />
+
+    <ThemeProvider theme={lightTheme}>
+      App File
+    </ThemeProvider>
+
+
+  </>
+
 }
 
-export default App
+export default App;
 
