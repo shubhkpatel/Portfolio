089ccc0a8c2cf3078c1c604f14c2496092f7abea
Shubh <patelshubh694@gmail.com>
2021-12-31 12:51:34 +0530

    Converted all SVGs into react component using SVG Playground & Designed the Home Button

--- portfolio-website/src/components/Main.js
+++ portfolio-website/src/components/Main.js
@@ -1,9 +1,31 @@
+import styled from "styled-components";
+import PowerButton from "../subComponents/PowerButton";
+
 const Main = () => {
     return (
-        <div>
-            Main Component/Page
-        </div>
+        <MainContainer>
+            <Container>
+                <PowerButton />
+            </Container>
+        </MainContainer>
     )
 }
 
 export default Main;
+
+const MainContainer = styled.div`
+    background: ${props => props.theme.body};
+    width: 100vw;
+    height: 100vh;
+    overflow: hidden;
+    position: relative;
+
+    h2,h3,h4,h5,h6{
+        font-family:'Karla', sans-serif ;
+        font-weight:500;
+    }
+`;
+
+const Container = styled.div`
+    padding: 2rem;
+`;
