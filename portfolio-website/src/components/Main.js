4e044fd07667c4da8d5530310432aa2e66d081e1
Shubh <patelshubh694@gmail.com>
2021-12-31 15:41:04 +0530

    Added the Contact Section

--- portfolio-website/src/components/Main.js
+++ portfolio-website/src/components/Main.js
@@ -1,3 +1,4 @@
+import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 import LogoComponent from "../subComponents/LogoComponent";
 import PowerButton from "../subComponents/PowerButton";
@@ -10,8 +11,12 @@ const Main = () => {
                 <PowerButton />
                 <LogoComponent />
                 <SocialIcons />
+
+                <Contact target="_blank" to={{ pathname: "mailto:patelshubh694@gmail.com" }} >
+                    <h3>Say hi..</h3>
+                </Contact>
             </Container>
-        </MainContainer>
+        </MainContainer >
     )
 }
 
@@ -32,4 +37,13 @@ const MainContainer = styled.div`
 
 const Container = styled.div`
     padding: 2rem;
+`;
+
+const Contact = styled(NavLink)`
+    color: ${props => props.theme.text};
+    position: absolute;
+    top: 2rem;
+    right: calc(1rem + 2vw);
+    text-decoration: none;
+    z-index: 1;
 `;
