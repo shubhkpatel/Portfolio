c2afcb2656f2eb5f60bdf8bff43148ed342327ce
Shubh <patelshubh694@gmail.com>
2021-12-31 16:06:20 +0530

    Added Links to Different Page - Projects, Works, About, My Skills

--- portfolio-website/src/components/Main.js
+++ portfolio-website/src/components/Main.js
@@ -13,8 +13,27 @@ const Main = () => {
                 <SocialIcons />
 
                 <Contact target="_blank" to={{ pathname: "mailto:patelshubh694@gmail.com" }} >
-                    <h3>Say hi..</h3>
+                    <h2>Say hi..</h2>
                 </Contact>
+
+                <WORK to="/work" >
+                    <h2>Work</h2>
+                </WORK>
+
+                <PROJECT to="/projects" >
+                    <h2>Project</h2>
+                </PROJECT>
+
+                <BottomBar>
+                    <ABOUT to="/about" >
+                        <h2>About.</h2>
+                    </ABOUT>
+
+                    <SKILLS to="/skills" >
+                        <h2>My Skills.</h2>
+                    </SKILLS>
+                </BottomBar>
+
             </Container>
         </MainContainer >
     )
@@ -46,4 +65,46 @@ const Contact = styled(NavLink)`
     right: calc(1rem + 2vw);
     text-decoration: none;
     z-index: 1;
+`;
+
+const WORK = styled(NavLink)`
+    color: ${props => props.theme.text};
+    position: absolute;
+    top: 50%;
+    transform: rotate(90deg) translate(-50%, -50%);
+    right: calc(1rem + 2vw);
+    text-decoration: none;
+    z-index: 1;
+`;
+
+const PROJECT = styled(NavLink)`
+    color: ${props => props.theme.text};
+    position: absolute;
+    top: 50%;
+    transform: translate(-50%, -50%) rotate(-90deg);
+    left: calc(1rem + 2vw);
+    text-decoration: none;
+    z-index: 1;
+`;
+
+const BottomBar = styled.div`
+    position: absolute;
+    bottom: 1rem;
+    left: 0;
+    right: 0;
+    width: 100%;
+    display: flex;
+    justify-content: space-evenly;
+`;
+
+const ABOUT = styled(NavLink)`
+    color: ${props => props.theme.text};
+    text-decoration: none;
+    z-index:1;
+`;
+
+const SKILLS = styled(NavLink)`
+    color: ${props => props.theme.text};
+    text-decoration: none;
+    z-index:1;
 `;
